fd74a4172225fed619899ad111e364f0616de8fb
ebrahim969 <ebrahimelnemr084@gmail.com>
2023-08-28 22:36:33 +0300

    progect created and create splash view feature

---
+++ android/app/src/main/kotlin/com/example/ay_khedma/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ay_khedma
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'features/splash/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashView(),
+    );
+  }
+}

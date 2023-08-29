d5a6994f8d3acd339503f88812716f52890fe6a6
ebrahim969 <ebrahimelnemr084@gmail.com>
2023-08-29 19:11:06 +0300

    change app name and add router config

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'features/splash/presentation/views/splash_view.dart';
+import 'core/utils/app_router.dart';
 
 void main() {
-  runApp(const BooklyApp());
+  runApp(const AyKhedmaApp());
 }
 
-class BooklyApp extends StatelessWidget {
-  const BooklyApp({super.key});
+class AyKhedmaApp extends StatelessWidget {
+  const AyKhedmaApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return  MaterialApp.router(
+      routerConfig: AppRouter.router,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(textTheme: GoogleFonts.rubikTextTheme()),
-      home: const SplashView(),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:ay_khedma/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const BooklyApp());
+    await tester.pumpWidget(const AyKhedmaApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);

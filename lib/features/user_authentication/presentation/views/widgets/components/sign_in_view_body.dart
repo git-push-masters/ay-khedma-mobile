c75e58ba48c5ec37951f57e3c377b2bce887468f
ebrahim969 <ebrahimelnemr084@gmail.com>
2023-08-31 04:47:39 +0300

    add localization correctly

--- lib/features/user_authentication/presentation/views/widgets/components/sign_in_view_body.dart
+++ lib/features/user_authentication/presentation/views/widgets/components/sign_in_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:ay_khedma/core/helper/widgets/custom_text_field.dart';
+import 'package:ay_khedma/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 import '../sections/header_signin_view_section.dart';
@@ -14,11 +16,28 @@ class SignInViewBody extends StatelessWidget {
           alignment: Alignment.center,
           child: Column(
             children:const[
-              HeaderSignInViewSection()
-              
+              HeaderSignInViewSection(),
+             SizedBox(height:55 ,),
+             SignInUserSection(),
             ],
           ),
         ),
       ) ,);
+  }
+}
+
+class SignInUserSection extends StatelessWidget {
+  const SignInUserSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children:const [
+        Text("الأميل", style: Styles.textStyle12,),
+        CustomTextFeild(hinttext: "أدخل الأميل الخاص بك"),
+        Text("كلمه المرور", style: Styles.textStyle12,),
+        CustomTextFeild(hinttext: "أدخل كلمه المرور الخاصه بك"),
+      ],
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'core/utils/app_router.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const AyKhedmaApp());
@@ -12,11 +12,21 @@ class AyKhedmaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      localizationsDelegates:const [
+    GlobalMaterialLocalizations.delegate,
+    GlobalWidgetsLocalizations.delegate,
+    GlobalCupertinoLocalizations.delegate,
+  ],
+  supportedLocales:const [
+    Locale('en'), // English
+    Locale('ar'), // Spanish
+  ],
       routerConfig: AppRouter.router,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'Kohinoor Arabic',
-        textTheme: Theme.of(context).textTheme.apply(fontFamily: 'Kohinoor Arabic'),
+        textTheme:
+            Theme.of(context).textTheme.apply(fontFamily: 'Kohinoor Arabic'),
       ),
     );
   }

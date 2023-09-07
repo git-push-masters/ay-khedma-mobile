d83847e13cf6961de90972c8de584ef883664b36
mustfa-magdy-mohamed-nasr <Mostfammaaggddyy@gmail.com>
2023-09-07 17:36:10 +0300

    create find location view done

--- lib/features/find_location/presentation/views/widgets/find_location_view_body.dart
+++ lib/features/find_location/presentation/views/widgets/find_location_view_body.dart
@@ -1,10 +1,31 @@
+import 'package:ay_khedma/core/helper/widgets/custom_button.dart';
+import 'package:ay_khedma/core/utils/assets.dart';
+import 'package:ay_khedma/core/utils/colors.dart';
+import 'package:ay_khedma/core/utils/styles.dart';
+import 'package:ay_khedma/features/find_location/presentation/views/widgets/secions/FindLocation_View_Center_Section.dart';
+import 'package:ay_khedma/features/find_location/presentation/views/widgets/secions/FindLocation_View_Header_Section.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
 
 class FindLocationViewBody extends StatelessWidget {
   const FindLocationViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+        child: Column(
+      children:  [
+        const FindLocationViewHeaderSection(),
+        SizedBox(height: Get.height*.15,),
+        const FindLocationViewCenterSection(),
+      ],
+    ));
   }
 }
+
+
+
+
+
+   
---
+++ lib/features/find_location/presentation/views/widgets/secions/FindLocation_View_Header_Section.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../../../../../core/utils/assets.dart';
+import '../../../../../../core/utils/colors.dart';
+
+class FindLocationViewHeaderSection extends StatelessWidget {
+  const FindLocationViewHeaderSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Positioned(left: 0, child: Image.asset(AssetsData.findLocationHeader)),
+        Align(
+            alignment: Alignment.centerRight,
+            child: IconButton(
+                onPressed: () {
+                  GoRouter.of(context).pop();
+                },
+                icon: const Icon(FontAwesomeIcons.arrowRight, color: AppColors.kPrimaryColor,))),
+      ],
+    );
+  }
+}

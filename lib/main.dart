bb7580b6d76ce03b1db92321e076dcbcc45dc2d5
ebrahim969 <ebrahimelnemr084@gmail.com>
2023-09-14 18:40:05 +0300

    bloc observe and future builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,12 @@
 import 'package:ay_khedma/core/helper/helper_services/service_locator.dart';
-import 'package:ay_khedma/features/home/presentation/views/all_services_view.dart';
-import 'package:ay_khedma/features/home/presentation/views/home_view.dart';
 import 'package:ay_khedma/features/splash/presentation/views/splash_view.dart';
 import 'package:ay_khedma/features/user_authentication/data/repos/user_auth_repo_implement.dart';
 import 'package:ay_khedma/features/user_authentication/presentation/view_models/cubits/register_cubit/register_cubit.dart';
-import 'package:ay_khedma/features/user_authentication/presentation/views/sign_in_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'core/helper/auth_contain.dart';
-import 'core/helper/test_api.dart';
-import 'core/utils/app_router.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'features/Appointments/presentation/views/widgets/sectiones/appointments_view.dart';
-import 'features/chats_and_calls/presentation/views/chat_view.dart';
-import 'features/chats_and_calls/presentation/views/widgets/chat_view_body.dart';
-import 'features/notifications/presentation/views/widgets/notifications_view.dart';
-import 'features/profile/presentation/views/profile_view.dart';
 import 'features/user_authentication/presentation/view_models/cubits/cubit/login_cubit.dart';
-import 'features/user_authentication/presentation/views/sign_up_view.dart';
 
 void main() {
   setupServiceLocator();
@@ -54,7 +41,7 @@ class AyKhedmaApp extends StatelessWidget {
           Locale('ar'),
         ],
         //routerConfig: AppRouter.router,
-        home: const HomeView(),
+        home: const SplashView(),
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           fontFamily: 'Kohinoor Arabic',
